a4706158918e4ed0ff5d7d48e0e790b4c183dcae
Cyterro <up2282407@myport.ac.uk>
2024-11-11 18:19:43 +0000

    settings menu completed
    its complete needs a bit fiddling with but  its done

--- Assets/Scripts/menu scripts/SettingsMenu.cs
+++ Assets/Scripts/menu scripts/SettingsMenu.cs
@@ -12,7 +12,7 @@ public class SettingsMenu : MonoBehaviour
 
     Resolution[] resolutions;
 
-    void start () 
+    void Start() 
     { 
         resolutions = Screen.resolutions;
 
@@ -20,13 +20,32 @@ public class SettingsMenu : MonoBehaviour
 
         List<string> options = new List<string>();   // creates a list of strings for our new resolution options
 
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)   // we loop through each 'element' in our resolutions array
         {
             string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + " Hz";
             options.Add(option); // before adding it to our options list
-        }
 
+            /*if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            } 
+            */
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
         resolutionDropdown.AddOptions(options); // once the loop is complete we'll add the list to our resolutions dropdown box
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetVolume(float volume)
